eda4dd296c94822300d3e737013d297ae31c9b3c
Tim11Larsen <x98larsen@yahoo.com>
2026-02-08 15:00:47 -0500

    final

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-console.log("JavaScript is working!");
-
 function openMenu() {
   document.body.classList += " menu--open";
 }
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
-
-function updatePlaceholder() {
-    const input = document.getElementById("header__email--input");
-    if (window.innerWidth <= 500) { // Adjust the breakpoint as needed
-        input.placeholder = "Email"; // Change to smaller placeholder
-    } else {
-        input.placeholder = "Your email address"; // Original placeholder
-    }
-}
-
-window.onload = updatePlaceholder;
-window.onresize = updatePlaceholder;
