62dd1eb21744d5830669af33f576b1cf3539426d
Tim11Larsen <x98larsen@yahoo.com>
2026-02-08 13:22:55 -0500

    responsive

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ function closeMenu() {
   document.body.classList.remove("menu--open");
 }
 
-/* <button class="btn__menu" on click="openMenu()">
-                    <i class="fa-solid fa-bars"></i>
-                </button> */
+function updatePlaceholder() {
+    const input = document.getElementById("header__email--input");
+    if (window.innerWidth <= 500) { // Adjust the breakpoint as needed
+        input.placeholder = "Email"; // Change to smaller placeholder
+    } else {
+        input.placeholder = "Your email address"; // Original placeholder
+    }
+}
 
-                 /* <div class="menu">
-            <svg></svg>
-            <button class="btn__menu btn__menu--close" onclick="closeMenu()"></button>
-            <div class="menu__links">
-                <a href="#about" class="menu__link" onclick="closeMenu()">About</a>
-                <a href="#blog" class="menu__link" onclick="closeMenu()">Blog</a>
-                <a href="#pricing" class="menu__link" onclick="closeMenu()">Pricing</a>
-                <a href="#contact us" class="menu__link" onclick="closeMenu()">Contact Us</a>
-                <a href="#login" class="menu__link" onclick="closeMenu()">Login</a>
-                <a href="#signup" class="menu__link menu__link--primary" onclick="closeMenu()">Sign Up</a>
-            </div>
-        </div> */
+window.onload = updatePlaceholder;
+window.onresize = updatePlaceholder;
